4fce4d58e19b979bef9275d8a593e73aa6376d71
Theodore Karatapanis <Theodoros.Karatapanis@rt-rk.com>
2020-11-22 18:29:27 +0100

    First working Project

---
+++ app/src/main/java/com/example/currencycalculator/AsyncReq.java
@@ -0,0 +1,56 @@
+package com.example.currencycalculator;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+class AsyncReq extends AsyncTask<Void, Void, String> {
+
+    private Exception exception;
+    private HttpURLConnection connection;
+    private URL url;
+    String debug;
+    String response ;
+    boolean finished = false;
+    @Override
+    protected String doInBackground(Void... voids) {
+        BufferedReader reader;
+        String line;
+        response = "init";
+        StringBuffer responseContent = new StringBuffer();
+
+        try{
+            url = new URL("http://data.fixer.io/api/latest?access_key=7970f0a15d17e57b23dff860969bfa2f");
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+            int status = connection.getResponseCode();
+            if (status < 300)
+            {
+                reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                while ((line = reader.readLine()) != null)
+                {
+                    responseContent.append(line);
+                }
+                reader.close();
+            }
+            response = responseContent.toString();
+        }
+        catch(Exception e)
+        {
+         response = "failed";
+        }
+        finished = true;
+        return response;
+    }
+
+    protected void onPostExecute(Void nothing) {
+        // TODO: check this.exception
+        // TODO: do something with the feed
+
+    }
+}
---
+++ app/src/main/java/com/example/currencycalculator/MainActivity.java
@@ -0,0 +1,274 @@
+package com.example.currencycalculator;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
+
+import com.google.gson.JsonObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity  {
+
+    Button button0, button1, button2, button3, button4, button5, button6,
+            button7, button8, button9, buttonAdd, buttonSub, buttonDivision,
+            buttonMul, button10, buttonConvert, buttonEqual, buttonC;
+    Spinner spinner1, spinner2;
+    EditText crunchifyEditText;
+    Currencies apiCurs;
+    float mValueOne, mValueTwo;
+
+    boolean crunchifyAddition, mSubtract, crunchifyMultiplication, crunchifyDivision;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button0 =  findViewById(R.id.button0);
+        button1 =  findViewById(R.id.button1);
+        button2 =  findViewById(R.id.button2);
+        button3 =  findViewById(R.id.button3);
+        button4 =  findViewById(R.id.button4);
+        button5 =  findViewById(R.id.button5);
+        button6 =  findViewById(R.id.button6);
+        button7 =  findViewById(R.id.button7);
+        button8 =  findViewById(R.id.button8);
+        button9 =  findViewById(R.id.button9);
+        button10 = findViewById(R.id.button10);
+        buttonC = findViewById(R.id.buttonC);
+        buttonAdd = findViewById(R.id.buttonadd);
+        buttonSub = findViewById(R.id.buttonsub);
+        buttonMul = findViewById(R.id.buttonmul);
+        buttonDivision = findViewById(R.id.buttondiv);
+        buttonConvert = findViewById(R.id.buttonConvert);
+        buttonEqual = findViewById(R.id.buttoneql);
+        crunchifyEditText = findViewById(R.id.edt1);
+        Currencies curS = new Currencies();
+        apiCurs = curS;
+        addItemsOnSpinners();
+        addListenerOnSpinnerItemSelection();
+        addListenerOnButton();
+
+       /* ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, users);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        *//*spin.setAdapter(adapter);
+        spin.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);*/
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                crunchifyEditText.setText(crunchifyEditText.getText() + "1");
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                crunchifyEditText.setText(crunchifyEditText.getText() + "2");
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                crunchifyEditText.setText(crunchifyEditText.getText() + "3");
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                crunchifyEditText.setText(crunchifyEditText.getText() + "4");
+            }
+        });
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                crunchifyEditText.setText(crunchifyEditText.getText() + "5");
+            }
+        });
+
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                crunchifyEditText.setText(crunchifyEditText.getText() + "6");
+            }
+        });
+
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                crunchifyEditText.setText(crunchifyEditText.getText() + "7");
+            }
+        });
+
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                crunchifyEditText.setText(crunchifyEditText.getText() + "8");
+            }
+        });
+
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                crunchifyEditText.setText(crunchifyEditText.getText() + "9");
+            }
+        });
+
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                crunchifyEditText.setText(crunchifyEditText.getText() + "0");
+            }
+        });
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (crunchifyEditText == null) {
+                    crunchifyEditText.setText("");
+                } else {
+                    mValueOne = Float.parseFloat(crunchifyEditText.getText() + "");
+                    crunchifyAddition = true;
+                    crunchifyEditText.setText(null);
+                }
+            }
+        });
+
+        buttonSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(crunchifyEditText.getText() + "");
+                mSubtract = true;
+                crunchifyEditText.setText(null);
+            }
+        });
+
+        buttonMul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(crunchifyEditText.getText() + "");
+                crunchifyMultiplication = true;
+                crunchifyEditText.setText(null);
+            }
+        });
+
+        buttonDivision.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(crunchifyEditText.getText() + "");
+                crunchifyDivision = true;
+                crunchifyEditText.setText(null);
+            }
+        });
+        buttonC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                crunchifyEditText.setText("");
+            }
+        });
+        buttonEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueTwo = Float.parseFloat(crunchifyEditText.getText() + "");
+
+                if (crunchifyAddition == true) {
+                    crunchifyEditText.setText(mValueOne + mValueTwo + "");
+                    crunchifyAddition = false;
+                }
+
+                if (mSubtract == true) {
+                    crunchifyEditText.setText(mValueOne - mValueTwo + "");
+                    mSubtract = false;
+                }
+
+                if (crunchifyMultiplication == true) {
+                    crunchifyEditText.setText(mValueOne * mValueTwo + "");
+                    crunchifyMultiplication = false;
+                }
+
+                if (crunchifyDivision == true) {
+                    crunchifyEditText.setText(mValueOne / mValueTwo + "");
+                    crunchifyDivision = false;
+                }
+            }
+        });
+
+
+        button10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                crunchifyEditText.setText(crunchifyEditText.getText() + ".");
+            }
+        });
+
+        buttonConvert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fromCur = spinner1.getSelectedItem().toString();
+                String toCur = spinner2.getSelectedItem().toString();
+                float fromVal = apiCurs.rates.get(fromCur).getAsFloat();
+                float toVal = apiCurs.rates.get(toCur).getAsFloat();
+                float modifier = toVal / fromVal;
+                crunchifyEditText.setText(Float.parseFloat(crunchifyEditText.getText() + "") * modifier + "");
+            }
+        });
+       /* spin.setOnItemSelectedListener(new OnItemSelectedListener()
+        {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+            {
+                String selectedItem = parent.getItemAtPosition(position).toString();
+                if(selectedItem.equals("Add new category"))
+                {
+                    // do your stuff
+                }
+            } // to close the onItemSelected
+            public void onNothingSelected(AdapterView<?> parent)
+            {
+
+            }
+        });*/
+
+    }
+    public void addListenerOnSpinnerItemSelection() {
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        spinner2 = (Spinner) findViewById(R.id.spinner2);
+        spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+        spinner2.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+    }
+    // get the selected dropdown list value
+    public void addListenerOnButton() {
+
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        spinner2 = (Spinner) findViewById(R.id.spinner2);
+    }
+    public void addItemsOnSpinners() {
+
+        spinner2 = (Spinner) findViewById(R.id.spinner2);
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+
+        List<String> list = new ArrayList<String>();
+        for (String s:apiCurs.currencies)
+        {
+            list.add(s);
+        }
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(dataAdapter);
+        spinner1.setAdapter(dataAdapter);
+    }
+}
